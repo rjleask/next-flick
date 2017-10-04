763c157ee604540ba20c94e50cbd5b02a5ed7e72
Jamie <rjamie.leask@gmail.com>
2017-10-04 11:21:18 -0400

    working database

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var exphbs = require("express-handlebars");
 var path = require('path');
+var db = require("./models");
+
 app.use(express.static(path.join(__dirname, 'public')));
-
-// var db = require("./models");
-
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(express.static(__dirname + '/public/assets/css'));
@@ -15,13 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // // Override with POST having ?_method=DELETE
 // app.use(methodOverride("_method"));
+require("./routes/settings-api-posts.js")(app);
 require("./routes/movieController.js")(app);
 
 
 var PORT = process.env.PORT || 3000;
-// db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-// });
+});
 
